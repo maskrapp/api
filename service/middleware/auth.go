191438bde0a39fe664d100d648a804cd2045725f
Thomas Huisman <doedelenthomas@gmail.com>
2022-10-01 22:05:46 +0200

    fix: middleware

--- service/middleware/auth.go
+++ service/middleware/auth.go
@@ -1,38 +1,43 @@
 package middleware
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/maskrapp/backend/jwt"
 	"github.com/maskrapp/backend/models"
 )
 
-func AuthMiddleware(supabaseToken, baseURL string) func(*fiber.Ctx) error {
-	var endpoint = baseURL + "/auth/v1/user"
+func AuthMiddleware(jwtHandler *jwt.JWTHandler) func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		auth := c.GetReqHeaders()["Authorization"]
 		if auth == "" {
 			return c.SendStatus(401)
 		}
-		client := http.DefaultClient
-		request, _ := http.NewRequest("GET", endpoint, nil)
-		request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", auth))
-		request.Header.Set("apiKey", supabaseToken)
-		response, err := client.Do(request)
-		if err != nil || response.StatusCode != 200 {
-			return c.SendStatus(500)
-		}
-		usr := &models.User{}
-		err = json.NewDecoder(response.Body).Decode(&usr)
+		split := strings.Split(auth, " ")
+		accessToken := split[1]
+		claims, err := jwtHandler.Validate(accessToken, false)
 		if err != nil {
-			return c.SendStatus(500)
+			fmt.Println("err", err)
+			if strings.Contains(err.Error(), "token mismatch") {
+				return c.Status(400).JSON(&models.APIResponse{
+					Success: false,
+					Message: "Token mismatch",
+				})
+			}
+			if strings.Contains(err.Error(), "token is expired by") {
+				return c.Status(401).JSON(&models.APIResponse{
+					Success: false,
+					Message: "Token is expired",
+				})
+			}
+			return c.Status(400).JSON(&models.APIResponse{
+				Success: false,
+			})
+
 		}
-		if usr.Role != 0 {
-			return c.SendStatus(401)
-		}
-		c.Locals("user", usr)
+		c.Locals("user_id", claims.UserId)
 		return c.Next()
 	}
 }
