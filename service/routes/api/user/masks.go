f2c7a5c088271feb994adb4ae63f5932a6fe30cf
Thomas Huisman <contact@thohui.com>
2022-10-13 01:47:54 +0200

    refactor: use inner join

--- service/routes/api/user/masks.go
+++ service/routes/api/user/masks.go
@@ -16,7 +16,7 @@ func Masks(db *gorm.DB) func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		userID := c.Locals("user_id").(string)
 		masks := []record{}
-		err := db.Table("masks").Select("masks.mask, masks.enabled, emails.email").Joins("left join emails on emails.id = masks.forward_to").Where("emails.user_id = ?", userID).Find(&masks).Error
+		err := db.Table("masks").Select("masks.mask, masks.enabled, emails.email").Joins("inner join emails on emails.id = masks.forward_to").Where("emails.user_id = ?", userID).Find(&masks).Error
 		if err != nil {
 			return c.Status(500).JSON(&models.APIResponse{
 				Success: false,
