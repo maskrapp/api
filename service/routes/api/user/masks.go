40d701f10acddbc1f1cf53773c4583bbd77e3bd0
Thomas Huisman <contact@thohui.com>
2022-11-20 02:32:30 +0100

    feat: include stats in /masks endpoint response

--- service/routes/api/user/masks.go
+++ service/routes/api/user/masks.go
@@ -7,16 +7,18 @@ import (
 )
 
 type record struct {
-	Mask    string `json:"mask"`
-	Email   string `json:"email"`
-	Enabled bool   `json:"enabled"`
+	Mask              string `json:"mask"`
+	Email             string `json:"email"`
+	Enabled           bool   `json:"enabled"`
+	MessagesReceived  int    `json:"messages_received"`
+	MessagesForwarded int    `json:"messages_forwarded"`
 }
 
 func Masks(db *gorm.DB) func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		userID := c.Locals("user_id").(string)
 		masks := []record{}
-		err := db.Table("masks").Select("masks.mask, masks.enabled, emails.email").Joins("inner join emails on emails.id = masks.forward_to").Where("emails.user_id = ?", userID).Find(&masks).Error
+		err := db.Table("masks").Select("masks.mask, masks.enabled, masks.messages_forwarded, masks.messages_received, emails.email").Joins("inner join emails on emails.id = masks.forward_to").Where("emails.user_id = ?", userID).Find(&masks).Error
 		if err != nil {
 			return c.Status(500).JSON(&models.APIResponse{
 				Success: false,
