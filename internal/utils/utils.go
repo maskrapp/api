a8389db7045a11deac99dff2eec30c3bbd9b290c
Thomas Huisman <contact@thohui.com>
2023-01-13 01:36:03 +0100

    refactor: remove unused code

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,21 +2,13 @@ package utils
 
 import (
 	"math/rand"
-	"strconv"
 	"strings"
 )
-
-func ConvertWithFallback(value string, fallback int) int {
-	val, err := strconv.Atoi(value)
-	if err != nil {
-		return fallback
-	}
-	return val
-}
 
 var set = "1234567890"
 
 func GenerateCode(length int) string {
+
 	sb := strings.Builder{}
 	sb.Grow(length)
 	for i := 0; i < length; i++ {
