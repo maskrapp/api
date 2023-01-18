8798eaf691549e13dbc8abc27e39bd49bc82f9dd
Thomas Huisman <contact@thohui.com>
2023-01-18 17:04:32 +0100

    feat(grpc): mask validation

--- internal/grpc/grpc.go
+++ internal/grpc/grpc.go
@@ -2,8 +2,10 @@ package grpc
 
 import (
 	"context"
+	"strings"
 
 	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/maskrapp/backend/internal/domains"
 	"github.com/maskrapp/backend/internal/global"
 	stubs "github.com/maskrapp/backend/internal/pb/backend/v1"
 	"google.golang.org/grpc/codes"
@@ -13,17 +15,28 @@ import (
 )
 
 type backendServiceImpl struct {
-	db *gorm.DB
+	db      *gorm.DB
+	domains *domains.Domains
 	stubs.UnimplementedBackendServiceServer
 }
 
 func NewBackendService(ctx global.Context) stubs.BackendServiceServer {
 	return &backendServiceImpl{
-		db: ctx.Instances().Gorm,
+		db:      ctx.Instances().Gorm,
+		domains: ctx.Instances().Domains,
 	}
 }
 
 func (b *backendServiceImpl) CheckMask(ctx context.Context, request *stubs.CheckMaskRequest) (*stubs.CheckMaskResponse, error) {
+	split := strings.Split(request.MaskAddress, "@")
+	if len(split) != 2 {
+		return nil, status.New(codes.InvalidArgument, "invalid mask address").Err()
+	}
+
+	if _, err := b.domains.Get(split[1]); err != nil {
+		return nil, status.New(codes.InvalidArgument, "invalid mask domain").Err()
+	}
+
 	var result struct {
 		Found bool
 	}
@@ -39,6 +52,16 @@ func (b *backendServiceImpl) CheckMask(ctx context.Context, request *stubs.Check
 }
 func (b *backendServiceImpl) GetMask(ctx context.Context, request *stubs.GetMaskRequest) (*stubs.GetMaskResponse, error) {
 	resp := &stubs.GetMaskResponse{}
+
+	split := strings.Split(request.MaskAddress, "@")
+	if len(split) != 2 {
+		return nil, status.New(codes.InvalidArgument, "invalid mask address").Err()
+	}
+
+	if _, err := b.domains.Get(split[1]); err != nil {
+		return nil, status.New(codes.InvalidArgument, "invalid mask domain").Err()
+	}
+
 	err := b.db.Table("masks").Select("masks.enabled, emails.email").Joins("inner join emails on emails.id = masks.forward_to").Where("masks.mask = ?", request.MaskAddress).Find(&resp).Error
 	if err != nil {
 		return nil, status.New(codes.Unavailable, err.Error()).Err()
@@ -46,6 +69,16 @@ func (b *backendServiceImpl) GetMask(ctx context.Context, request *stubs.GetMask
 	return resp, nil
 }
 func (b *backendServiceImpl) IncrementForwardedCount(ctx context.Context, request *stubs.IncrementForwardedCountRequest) (*emptypb.Empty, error) {
+
+	split := strings.Split(request.MaskAddress, "@")
+	if len(split) != 2 {
+		return nil, status.New(codes.InvalidArgument, "invalid mask address").Err()
+	}
+
+	if _, err := b.domains.Get(split[1]); err != nil {
+		return nil, status.New(codes.InvalidArgument, "invalid mask domain").Err()
+	}
+
 	err := b.db.Table("masks").Where("mask = ?", request.MaskAddress).Updates(map[string]interface{}{"messages_received": gorm.Expr("messages_received + ?", 1), "messages_forwarded": gorm.Expr("messages_forwarded + ?", 1)}).Error
 
 	if err != nil {
@@ -55,6 +88,16 @@ func (b *backendServiceImpl) IncrementForwardedCount(ctx context.Context, reques
 	return &emptypb.Empty{}, nil
 }
 func (b *backendServiceImpl) IncrementReceivedCount(ctx context.Context, request *stubs.IncrementReceivedCountRequest) (*emptypb.Empty, error) {
+
+	split := strings.Split(request.MaskAddress, "@")
+	if len(split) != 2 {
+		return nil, status.New(codes.InvalidArgument, "invalid mask address").Err()
+	}
+
+	if _, err := b.domains.Get(split[1]); err != nil {
+		return nil, status.New(codes.InvalidArgument, "invalid mask domain").Err()
+	}
+
 	err := b.db.Table("masks").Where("mask = ?", request.MaskAddress).UpdateColumn("messages_received", gorm.Expr("messages_received + ?", 1)).Error
 	if err != nil {
 		return &empty.Empty{}, status.New(codes.Unavailable, err.Error()).Err()
