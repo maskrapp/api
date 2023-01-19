fbb6b6ceb4950e4640cd428a6759b6c043f7743c
Thomas Huisman <contact@thohui.com>
2023-01-19 01:18:49 +0100

    micro(grpc): log errors

--- internal/grpc/grpc.go
+++ internal/grpc/grpc.go
@@ -8,6 +8,7 @@ import (
 	"github.com/maskrapp/backend/internal/domains"
 	"github.com/maskrapp/backend/internal/global"
 	stubs "github.com/maskrapp/backend/internal/pb/backend/v1"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -43,6 +44,7 @@ func (b *backendServiceImpl) CheckMask(ctx context.Context, request *stubs.Check
 	err := b.db.Raw("SELECT EXISTS(SELECT 1 FROM masks WHERE mask = ?) AS found",
 		request.MaskAddress).Scan(&result).Error
 	if err != nil {
+		logrus.Errorf("db error: %v", err)
 		return nil, status.New(codes.Unavailable, err.Error()).Err()
 	}
 	if !result.Found {
@@ -64,6 +66,7 @@ func (b *backendServiceImpl) GetMask(ctx context.Context, request *stubs.GetMask
 
 	err := b.db.Table("masks").Select("masks.enabled, emails.email").Joins("inner join emails on emails.id = masks.forward_to").Where("masks.mask = ?", request.MaskAddress).Find(&resp).Error
 	if err != nil {
+		logrus.Errorf("db error: %v", err)
 		return nil, status.New(codes.Unavailable, err.Error()).Err()
 	}
 	return resp, nil
@@ -82,6 +85,7 @@ func (b *backendServiceImpl) IncrementForwardedCount(ctx context.Context, reques
 	err := b.db.Table("masks").Where("mask = ?", request.MaskAddress).Updates(map[string]interface{}{"messages_received": gorm.Expr("messages_received + ?", 1), "messages_forwarded": gorm.Expr("messages_forwarded + ?", 1)}).Error
 
 	if err != nil {
+		logrus.Errorf("db error: %v", err)
 		return &empty.Empty{}, status.New(codes.Unavailable, err.Error()).Err()
 	}
 
@@ -100,6 +104,7 @@ func (b *backendServiceImpl) IncrementReceivedCount(ctx context.Context, request
 
 	err := b.db.Table("masks").Where("mask = ?", request.MaskAddress).UpdateColumn("messages_received", gorm.Expr("messages_received + ?", 1)).Error
 	if err != nil {
+		logrus.Errorf("db error: %v", err)
 		return &empty.Empty{}, status.New(codes.Unavailable, err.Error()).Err()
 	}
 	return &empty.Empty{}, nil
